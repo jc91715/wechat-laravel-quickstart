4fe397e5d33b718690270f92145e40605c1c7929
jc91715 <370234915@qq.com>
2019-04-15 16:19:30 +0800

    init

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->default('');
+
+            $table->string('openid')->default('');
+            $table->string('nickname')->default('');
+            $table->string('sex')->default('');
+            $table->string('language')->default('');
+            $table->string('city')->default('');
+            $table->string('province')->default('');
+            $table->string('country')->default('');
+            $table->string('headimgurl')->default('');
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
